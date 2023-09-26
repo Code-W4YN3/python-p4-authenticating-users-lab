3d8e879e68d7f87b319c075064506f9d4c871bd7
wayne-otido <otidowayne@gmail.com>
2023-09-26 14:39:10 +0300

    done

--- server/app.py
+++ server/app.py
@@ -27,6 +27,34 @@ class ClearSession(Resource):
 
         return {}, 204
 
+class Login(Resource):
+    def post(self):
+        user = User.query.filter(
+            User.username == request.get_json()['username']
+        ).first()
+
+        session['user_id'] = user.id
+        return jsonify(user.to_dict())
+    
+api.add_resource(Login, '/login')
+
+class CheckSession(Resource):
+    def get(self):
+        user = User.query.filter(User.id == session.get('user_id')).first()
+        if user:
+            return user.to_dict()
+        else:
+            return {}, 401
+
+api.add_resource(CheckSession, '/check_session')
+    
+class Logout(Resource):
+    def delete(self): 
+        session['user_id'] = None
+        return {'message': '204: No Content'}, 204
+
+api.add_resource(Logout, '/logout')
+
 class IndexArticle(Resource):
     
     def get(self):
